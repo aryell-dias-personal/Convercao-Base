9f0da382f9e7f333f6bba1c70a84a88af66b65e8
aldo <aldo@aldo-Inspiron-3443>
2016-09-05 16:09:16 -0300

    ..

--- Exercicio-Conversao-de-Base/src/testes/Teste.java
+++ Exercicio-Conversao-de-Base/src/testes/Teste.java
@@ -4,9 +4,10 @@ import conversor.ConversorBaseBigDecimal;
 
 public class Teste {
 	public static void main(String[] args) {
-		String numero = "1.1";
-		int bi = 10;
+		String numero = "11.1";
+		int bi = 16;
 		int bf = 2;
 		System.out.println(ConversorBaseBigDecimal.converterFracionario(numero, bi, bf, 10));
+
 	}
 }
